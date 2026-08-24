fac589ad8309dbe1a0067256e0669b08ff610688
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-25 00:49:18 +0500

    Make development APK updates installable

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,15 +6,27 @@ android {
     namespace = "pk.gov.punjab.sbp.padel"
     compileSdk = 35
 
+    signingConfigs {
+        create("devStable") {
+            storeFile = rootProject.file("dev-signing.keystore")
+            storePassword = "sbppadeldev"
+            keyAlias = "sbppadeldev"
+            keyPassword = "sbppadeldev"
+        }
+    }
+
     defaultConfig {
         applicationId = "pk.gov.punjab.sbp.padel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1-debug"
+        versionCode = 2
+        versionName = "0.2-debug"
     }
 
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("devStable")
+        }
         release {
             isMinifyEnabled = false
         }
