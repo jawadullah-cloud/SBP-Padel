55f7e4059705847d7453781c2c479700c05d6056
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-25 03:49:41 +0500

    Bump Android dev build to 0.9

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "pk.gov.punjab.sbp.padel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.8-debug"
+        versionCode = 9
+        versionName = "0.9-debug"
     }
 
     buildTypes {
