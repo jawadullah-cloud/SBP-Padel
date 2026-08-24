4d4274851c6a2e7bc7c690a399c7cfe897d8597b
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-25 03:42:32 +0500

    Bump Android debug build for app icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "pk.gov.punjab.sbp.padel"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.7-debug"
+        versionCode = 8
+        versionName = "0.8-debug"
     }
 
     buildTypes {
