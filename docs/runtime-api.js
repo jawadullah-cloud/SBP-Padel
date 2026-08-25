fde46faf685436ce9cd61a4d377c94889583637c
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-25 12:53:06 +0500

    Resolve player API from current LAN host

---
+++ docs/runtime-api.js
@@ -0,0 +1,9 @@
+(()=>{
+  'use strict';
+  const host=location.hostname;
+  if(!host)return;
+  const api=`${location.protocol}//${host}:8000/api/v1`;
+  const previous=localStorage.getItem('sbpPadelApiBase')||'';
+  if(previous!==api)localStorage.setItem('sbpPadelApiBase',api);
+  window.SBPApiBase=()=>`${location.protocol}//${location.hostname}:8000/api/v1`;
+})();
