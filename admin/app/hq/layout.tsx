64efec1ddae6eb28c6726c11e87ece958982d49f
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-26 04:10:04 +0500

    Keep venue actions inside page headers

--- admin/app/hq/layout.tsx
+++ admin/app/hq/layout.tsx
@@ -2,7 +2,5 @@ import {ReactNode} from 'react';
 import HQTools from './HQTools';
 import HQGate from './HQGate';
 import HQTabBridge from './HQTabBridge';
-import HQVenueEditShortcut from './HQVenueEditShortcut';
-import HQHomeRouteBridge from './HQHomeRouteBridge';
 import './hq-enhancements.css';
-export default function HQLayout({children}:{children:ReactNode}){return <HQGate><HQTabBridge/><HQHomeRouteBridge/><div className="hqLayoutFrame"><HQTools/><div className="hqLayoutContent"><HQVenueEditShortcut/>{children}</div></div></HQGate>}
+export default function HQLayout({children}:{children:ReactNode}){return <HQGate><HQTabBridge/><div className="hqLayoutFrame"><HQTools/><div className="hqLayoutContent">{children}</div></div></HQGate>}
