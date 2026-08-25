3a95e04a7bd2a4cf84aff209d4c2583619e487c6
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-26 01:34:30 +0500

    Stabilize shared HQ layout ownership

--- admin/app/hq/layout.tsx
+++ admin/app/hq/layout.tsx
@@ -2,4 +2,4 @@ import {ReactNode} from 'react';
 import HQTools from './HQTools';
 import HQGate from './HQGate';
 import './hq-enhancements.css';
-export default function HQLayout({children}:{children:ReactNode}){return <HQGate><HQTools/>{children}</HQGate>}
+export default function HQLayout({children}:{children:ReactNode}){return <HQGate><div className="hqLayoutFrame"><HQTools/><div className="hqLayoutContent">{children}</div></div></HQGate>}
