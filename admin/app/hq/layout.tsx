966810606aebb34d47a5bb7b668b11013f97a398
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-26 03:51:51 +0500

    Route HQ home staff tab to staff management

--- admin/app/hq/layout.tsx
+++ admin/app/hq/layout.tsx
@@ -3,5 +3,6 @@ import HQTools from './HQTools';
 import HQGate from './HQGate';
 import HQTabBridge from './HQTabBridge';
 import HQVenueEditShortcut from './HQVenueEditShortcut';
+import HQHomeRouteBridge from './HQHomeRouteBridge';
 import './hq-enhancements.css';
-export default function HQLayout({children}:{children:ReactNode}){return <HQGate><HQTabBridge/><div className="hqLayoutFrame"><HQTools/><div className="hqLayoutContent"><HQVenueEditShortcut/>{children}</div></div></HQGate>}
+export default function HQLayout({children}:{children:ReactNode}){return <HQGate><HQTabBridge/><HQHomeRouteBridge/><div className="hqLayoutFrame"><HQTools/><div className="hqLayoutContent"><HQVenueEditShortcut/>{children}</div></div></HQGate>}
