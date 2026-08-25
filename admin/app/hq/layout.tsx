3664dffc9499a15d15e8a19e90fda64aeacfcf96
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-26 03:45:34 +0500

    Keep HQ tab navigation synchronized

--- admin/app/hq/layout.tsx
+++ admin/app/hq/layout.tsx
@@ -1,5 +1,6 @@
 import {ReactNode} from 'react';
 import HQTools from './HQTools';
 import HQGate from './HQGate';
+import HQTabBridge from './HQTabBridge';
 import './hq-enhancements.css';
-export default function HQLayout({children}:{children:ReactNode}){return <HQGate><div className="hqLayoutFrame"><HQTools/><div className="hqLayoutContent">{children}</div></div></HQGate>}
+export default function HQLayout({children}:{children:ReactNode}){return <HQGate><HQTabBridge/><div className="hqLayoutFrame"><HQTools/><div className="hqLayoutContent">{children}</div></div></HQGate>}
