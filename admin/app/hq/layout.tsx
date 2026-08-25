7b034cdc052cc0106d41652a8b11b39da6da6bb0
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-26 00:37:35 +0500

    Load HQ enhancement styles

--- admin/app/hq/layout.tsx
+++ admin/app/hq/layout.tsx
@@ -1,4 +1,5 @@
 import {ReactNode} from 'react';
 import HQTools from './HQTools';
 import HQGate from './HQGate';
+import './hq-enhancements.css';
 export default function HQLayout({children}:{children:ReactNode}){return <HQGate><HQTools/>{children}</HQGate>}
