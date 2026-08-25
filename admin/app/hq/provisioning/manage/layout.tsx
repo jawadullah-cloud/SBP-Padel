101f3b2a8875fbfe453f3303d50cf1f22bf1fde4
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-26 00:36:05 +0500

    Add venue gallery access to management

---
+++ admin/app/hq/provisioning/manage/layout.tsx
@@ -0,0 +1,3 @@
+import {ReactNode} from 'react';
+import GalleryAccess from './GalleryAccess';
+export default function ManageLayout({children}:{children:ReactNode}){return <><GalleryAccess/>{children}</>}
