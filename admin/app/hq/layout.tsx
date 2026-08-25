52f269891198c085b336c9f748ca15c952a5937d
jawadullah-cloud <jawadullah24@gmail.com>
2026-08-26 03:47:40 +0500

    Show venue profile shortcut inside management

--- admin/app/hq/layout.tsx
+++ admin/app/hq/layout.tsx
@@ -2,5 +2,6 @@ import {ReactNode} from 'react';
 import HQTools from './HQTools';
 import HQGate from './HQGate';
 import HQTabBridge from './HQTabBridge';
+import HQVenueEditShortcut from './HQVenueEditShortcut';
 import './hq-enhancements.css';
-export default function HQLayout({children}:{children:ReactNode}){return <HQGate><HQTabBridge/><div className="hqLayoutFrame"><HQTools/><div className="hqLayoutContent">{children}</div></div></HQGate>}
+export default function HQLayout({children}:{children:ReactNode}){return <HQGate><HQTabBridge/><div className="hqLayoutFrame"><HQTools/><div className="hqLayoutContent"><HQVenueEditShortcut/>{children}</div></div></HQGate>}
